2bd607bc4f4fa17ad939ac415b9ba28d22de5e82
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2014-11-05 20:24:54 -0500

    added show

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::TodosController < ApplicationController
   def index
     @todos = Todo.all
-    render :json
+  end
+
+  def show
+    @todo = todo.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @todo}
+    end
   end
 
   def create
